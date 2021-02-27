a61c20e8eaded4e6f9cf3ef29a16dd41e081b6ed
mke2111 <roymke134@gmail.com>
2021-02-27 18:27:08 +0300

    feat: add more text to credits' scene

--- src/Scenes/CreditsScene.js
+++ src/Scenes/CreditsScene.js
@@ -8,7 +8,8 @@ export default class CreditsScene extends Phaser.Scene {
  
   create () {
     this.creditsText = this.add.text(0, 0, 'Credits', { fontSize: '32px', fill: '#fff' });
-    this.madeByText = this.add.text(0, 0, 'Created By: Placeholder', { fontSize: '26px', fill: '#fff' });
+    this.madeByText = this.add.text(0, 0, 'Created By: Roy Mukuye', { fontSize: '26px', fill: '#fff' });
+    this.infoByText = this.add.text(0, 0, 'This RPG game is created using Phaser-3', { fontSize: '22px', fill: '#f00'});
     this.zone = this.add.zone(config.width/2, config.height/2, config.width, config.height);
     
     Phaser.Display.Align.In.Center(
@@ -20,8 +21,14 @@ export default class CreditsScene extends Phaser.Scene {
       this.madeByText,
       this.zone
     );
+
+    Phaser.Display.Align.In.Center(
+      this.infoByText,
+      this.zone
+    );
     
     this.madeByText.setY(1000);
+    this.infoByText.setY(1000);
 
     this.creditsTween = this.tweens.add({
       targets: this.creditsText,
@@ -37,6 +44,16 @@ export default class CreditsScene extends Phaser.Scene {
     this.madeByTween = this.tweens.add({
       targets: this.madeByText,
       y: -300,
+      ease: 'Power1',
+      duration: 8000,
+      onComplete: function () {
+        this.destroy;
+      }
+    });
+
+    this.infoByTween = this.tweens.add({
+      targets: this.infoByText,
+      y: -100,
       ease: 'Power1',
       duration: 8000,
       delay: 1000,
