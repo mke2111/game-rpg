2849432c0f786f3c3169cbb70c66469b7e3208da
mke2111 <roymke134@gmail.com>
2021-03-05 09:30:07 +0300

    update phaser dimensions

--- src/Config/config.js
+++ src/Config/config.js
@@ -5,10 +5,10 @@ import PhaserMatterCollisionPlugin from 'phaser-matter-collision-plugin';
 export default {
   type: Phaser.AUTO,
   parent: 'content',
-  // width: 650,
-  // height: 350,
-  width: 512,
-  height: 384,
+  width: 650,
+  height: 350,
+  // width: 512,
+  // height: 384,
   backgroundColor: '#000035',
   scale: {
     zoom: 1.5,
