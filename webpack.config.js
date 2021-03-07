9763d27f2a39dd3be66a96d1c3762b69c6915db3
mke2111 <roymke134@gmail.com>
2021-03-07 12:01:25 +0300

    add build folder

--- webpack.config.js
+++ webpack.config.js
@@ -8,8 +8,8 @@ module.exports = {
   entry: './src/index.js',
 
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    publicPath: '/dist/',
+    path: path.resolve(__dirname, 'build'),
+    publicPath: '/build/',
     filename: 'project.bundle.js'
   },
   
