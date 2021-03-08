dc36bdceb1601461b0918b34f67834d1ec30255a
mke2111 <roymke134@gmail.com>
2021-03-08 08:01:39 +0300

    update server.js file

--- server.js
+++ server.js
@@ -1,11 +1,29 @@
-var express = require('express');
-var app = express();
-var path = require('path');
+// var express = require('express');
+// var app = express();
+// var path = require('path');
 
-app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname, 'index.html'));
+// app.get('/', function (req, res) {
+//     res.sendFile(path.join(__dirname, 'index.html'));
+// });
+
+// app.listen(process.env.PORT || 4000, function () {
+//     console.log('Node app is working!');
+// });
+
+const path = require('path');
+const express = require('express');
+
+const app = express();
+const DIST_DIR = path.join(__dirname, '/build');
+const HTML_FILE = path.join(DIST_DIR, 'index.html');
+
+app.use(express.static(DIST_DIR));
+app.get('*', (req, res) => {
+  res.sendFile(HTML_FILE);
 });
 
-app.listen(process.env.PORT || 4000, function () {
-    console.log('Node app is working!');
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, () => {
+
 });
