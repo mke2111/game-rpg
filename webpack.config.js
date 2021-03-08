ace18c7797adc31a2cd30edb4615f0d42a452b33
mke2111 <roymke134@gmail.com>
2021-03-08 22:56:40 +0300

    update webpack config file

--- webpack.config.js
+++ webpack.config.js
@@ -21,17 +21,17 @@ module.exports = {
         test: [/\.vert$/, /\.frag$/],
         use: 'raw-loader'
       },
-      {
-        test: /\.m?js$/,
-        exclude: /(node_modules|bower_components)/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env'],
-            plugins: ['@babel/plugin-proposal-class-properties'],
-          },
-        },
-      },
+      // {
+      //   test: /\.m?js$/,
+      //   exclude: /(node_modules|bower_components)/,
+      //   use: {
+      //     loader: 'babel-loader',
+      //     options: {
+      //       presets: ['@babel/preset-env'],
+      //       plugins: ['@babel/plugin-proposal-class-properties'],
+      //     },
+      //   },
+      // },
       {
         test: /\.(png|jpe?g|gif|wav|mp3)$/i,
         use: [
